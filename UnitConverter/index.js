98250c5b8d9ede546eaf922bff5dbcfcb24c156d
AbyG1 <abygeorge133@gmail.com>
2023-12-11 16:27:40 +0530

    Add calculateMass and calculateVolume functiohns

--- UnitConverter/index.js
+++ UnitConverter/index.js
@@ -8,24 +8,44 @@ const valueEl = document.getElementById('input-box')
 const lengthEl = document.getElementById('length')
 const volumeEl = document.getElementById('volume')
 const massEl = document.getElementById('mass')
+let footValue
+let meterValue
+let literValue
+let gallonValue
+let kilogramValue
+let poundValue
 
 
 const buttonEl = document.getElementById('btn')
 buttonEl.addEventListener('click',() => {
     let inputValue = valueEl.value
     calculatelength(inputValue)
+    calculateVolume(inputValue)
+    calculateMass(inputValue)
+    render(inputValue)
 })
 
 function calculatelength(value){
-    let footValue = value * 3.281
-    footValue = Math.round(footValue * 1000)/1000
-    let meterValue = value/3.281
-    meterValue = Math.round(meterValue * 1000)/1000
-    render(footValue, meterValue)
-   }
+    footValue = Math.round(value * 3.281 * 1000)/1000
+    meterValue = Math.round(value / 3.281 * 1000)/1000
+}
+
+function calculateVolume(value){
+    literValue = Math.round(value * 0.264 * 1000)/1000
+    gallonValue = Math.round(value / 0.264 * 1000)/1000
+}
+
+function calculateMass(value){
+    kilogramValue = Math.round(value * 2.204 * 1000)/1000
+    poundValue = Math.round(value /2.204 * 1000)/1000
+}
 
 
-   function render(value1,value2){
-        let inputValue = valueEl.value
-        lengthEl.textContent = `${inputValue} meters = ${value1} feet | ${inputValue} feet = ${value2} meters `
+
+   function render(value){
+    
+        lengthEl.textContent = `${value} meters = ${footValue} feet | ${value} feet = ${meterValue} meters `
+        volumeEl.textContent = `${value} liters = ${gallonValue} gallons | ${value} gallons = ${literValue} liters`
+        massEl.textContent = `${value} kilograms = ${poundValue} pounds | ${value} pounds = ${kilogramValue} kilograms`
+
    }
