9d24fdb40f1a820edf4c24263e77206fbc679177
AbyG1 <abygeorge133@gmail.com>
2023-12-24 00:50:47 +0530

    Connect firebase

--- Endorsement-project/index.js
+++ Endorsement-project/index.js
@@ -1,23 +1,50 @@
 // javascript
-const btnEl = document.getElementById('btn')
+import { initializeApp } from'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js'
+import { getDatabase,ref,push } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js'
+
+const appSettings = {
+    databaseURL: "https://endorsements-database-e84b7-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+
+const app = initializeApp(appSettings)
+
+const database = getDatabase(app)
+
+const endorsementlistInDB = ref(database, "Endorsements") 
+
+
+
+const publishBtn = document.getElementById('btn')
 const endorsementContainer = document.getElementById('endorsements')
 const userCommentEl = document.getElementById('comment')
 
 
-btnEl.addEventListener('click',() => {
+publishBtn.addEventListener('click',() => {
+    
     const userInputText = userCommentEl.value
-    const endorsementEl = document.createElement('div')
-    endorsementEl.classList.add('endo-comments');
-    endorsementEl.textContent = userInputText
-    if(endorsementContainer.firstChild){
-        endorsementContainer.insertBefore(endorsementEl, endorsementContainer.firstChild)
+    if(userInputText.trim() != ''){
+        push(endorsementlistInDB,userInputText)
+        clearTextBox()
+    } else {
+        alert('Enter something')
     }
-    else {
-        endorsementContainer.appendChild(endorsementEl)
-            
-    }
-    clearTextBox()
 })
+
+
+// const endorsementEl = document.createElement('div')
+//     endorsementEl.classList.add('endo-comments');
+//     endorsementEl.textContent = userInputText
+//     if(endorsementContainer.firstChild){
+//         endorsementContainer.insertBefore(endorsementEl, endorsementContainer.firstChild)
+//     }
+//     else {
+//         endorsementContainer.appendChild(endorsementEl)
+            
+//     }
+   
+
+
 
 function clearTextBox() {
     userCommentEl.value = ''
