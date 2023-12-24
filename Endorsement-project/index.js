3960d03e06108efa0d9e54494a16f715284c0b7c
AbyG1 <abygeorge133@gmail.com>
2023-12-24 18:35:34 +0530

    Fix bugs

--- Endorsement-project/index.js
+++ Endorsement-project/index.js
@@ -1,6 +1,6 @@
 // javascript
 import { initializeApp } from'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js'
-import { getDatabase,ref,push } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js'
+import { getDatabase,ref,push,onValue } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js'
 
 const appSettings = {
     databaseURL: "https://endorsements-database-e84b7-default-rtdb.asia-southeast1.firebasedatabase.app/"
@@ -18,6 +18,9 @@ const endorsementlistInDB = ref(database, "Endorsements")
 const publishBtn = document.getElementById('btn')
 const endorsementContainer = document.getElementById('endorsements')
 const userCommentEl = document.getElementById('comment')
+const endorsementEl = document.createElement('div')
+
+
 
 
 publishBtn.addEventListener('click',() => {
@@ -31,18 +34,55 @@ publishBtn.addEventListener('click',() => {
     }
 })
 
+onValue(endorsementlistInDB,(snapshot) =>  {
 
-// const endorsementEl = document.createElement('div')
-//     endorsementEl.classList.add('endo-comments');
-//     endorsementEl.textContent = userInputText
-//     if(endorsementContainer.firstChild){
-//         endorsementContainer.insertBefore(endorsementEl, endorsementContainer.firstChild)
-//     }
-//     else {
-//         endorsementContainer.appendChild(endorsementEl)
+    if(snapshot.exists()){
+        
+        let endorsementArray = Object.entries(snapshot.val())
+        endorsementEl.innerHTML = ""
+
+           for(let i = 0; i < endorsementArray.length; i++){
+                let currentItem = endorsementArray[i]
+                let currentItemId = currentItem[0]
+                let currentItemValue = currentItem[1]
+           
+                displayEndorsement(currentItemValue)
+           }
+          
+
+    }
+           
+           
+        
+    })
+
+
+
+
+function displayEndorsement(value){
+
+    const para = document.createElement('p')
+
+    
+    para.classList.add('endo-comments');
+    para.textContent = value 
+    endorsementEl.appendChild(para)
+    if(endorsementContainer.firstChild){
+        endorsementContainer.insertBefore(endorsementEl, endorsementContainer.firstChild)
+    }
+    else {
+        endorsementContainer.appendChild(endorsementEl)
             
-//     }
-   
+    }
+
+
+
+
+}
+
+
+
+
 
 
 
